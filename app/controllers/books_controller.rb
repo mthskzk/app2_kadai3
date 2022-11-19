10ad40e51104863975e763fd294dc051a71dfedf
mthskzk <116724950+mthskzk@users.noreply.github.com>
2022-11-19 17:12:41 +0000

    [Add]能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,13 +5,6 @@ class BooksController < ApplicationController
     @book=Book.new
   end
 
-  def create
-    @book=Book.new(book_params)
-    @book.user_id=current_user.id
-    @book.save
-    redirect_to books_path
-  end
-
   def index
     @books=Book.all
   end
@@ -20,20 +13,43 @@ class BooksController < ApplicationController
     @book=Book.find(params[:id])
   end
 
+  def create
+    # 1,2. データを受け取り新規登録するためのインスタンス作成
+    @book=Book.new(book_params)
+
+    @book.user_id=current_user.id
+    # データが入力されているかのif文
+    if @book.save
+      flash[:notice]="Book was successfully created"
+      redirect_to book_path(@book.id)
+    else
+      # render index に行く前にindexにあるbooks変数に値をセットする
+      @books=Book.all
+      render :index
+    end
+  end
+
   def edit
     @book=Book.find(params[:id])
   end
 
   def update
     @book=Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
-
+    # データが入力され、アップデートされているかどうか
+    # データ入力されているとき
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully update"
+      redirect_to book_path(@book.id)
+    # データ入力されてないとき
+    else
+      render :edit
+    end
   end
 
   def destroy
     book=Book.find(params[:id])
     book.destroy
+    flash[:notie]="Book was successfully destroyed"
     redirect_to books_path
   end
 
